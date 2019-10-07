b047da69675898891a16a05edf71c9eb196d8f17
terfno <sueda.takahito@gmail.com>
2019-10-07 14:01:35 +0900

    :fire: add first

---
+++ gcdlcm.py
@@ -0,0 +1,37 @@
+import time
+
+
+def getTime():  # 現在時刻の取得関数
+    return time.time()
+
+
+def gcd(a, b):  # 最大公約数 gcd
+    if b != 0:
+        return gcd(b, a % b)
+    return a
+
+
+def lcm(ab, g):  # 最小公倍数 lcm
+    return ab/g
+
+
+def main():  # 最大公約数と最小公倍数を計算し、その結果と計算時間表示する
+    # 時間計測
+    startTime = getTime()
+
+    # 入力
+    inputnum = input("input 2 Integer: ").split(",")
+    a = int(inputnum[0])
+    b = int(inputnum[1])
+
+    # 計算
+    g = gcd(a, b)
+    l = lcm(a*b, g)
+
+    # 表示
+    print("gcd: "+str(g))
+    print("lcm: "+str(l))
+    print("calc time: " + str(getTime() - startTime) + "[ms]")
+
+
+main()
