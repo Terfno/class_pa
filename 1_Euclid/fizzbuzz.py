e485781a02364e75d1904a081f6602356ecd3340
terfno <sueda.takahito@gmail.com>
2019-10-29 09:04:28 +0900

    :zap: fix

--- 1_Euclid/fizzbuzz.py
+++ 1_Euclid/fizzbuzz.py
@@ -1,9 +1,10 @@
 fizz = input("fizz: ")
 buzz = input("buzz: ")
-a = int(input("num1: "))
-b = int(input("num2: "))
+a = int(input("num1 int: "))
+b = int(input("num2 int: "))
+maxnum = int(input("max int: "))
 
-for i in range(100):
+for i in range(maxnum):
     if(i % a):
         print(fizz)
         if(i % b):
