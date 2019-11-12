11d92db07ed72abc276c9406223058b4b963cfd8
terfno <sueda.takahito@gmail.com>
2019-11-12 15:14:30 +0900

    :bug:

--- 3_seatChange/seatChange.py
+++ 3_seatChange/seatChange.py
@@ -1,11 +1,9 @@
 import random
 
-# 30 31 43
-
 indexList = list(range(1, 46, 1))
 random.shuffle(indexList)
 
-index_eyeBad = []  # 30か31か43が入っているindexListの添字
+index_eyeBad = []
 
 for i in range(len(indexList)):
     if(indexList[i] == 30):
@@ -15,22 +13,24 @@ for i in range(len(indexList)):
     if(indexList[i] == 43):
         index_eyeBad.append(i)
 
+
+def r_rand():
+    r = random.randint(0, 17)
+    if(indexList[r] == 30 or indexList[r] == 31 or indexList[r] == 43):
+        r = r_rand()
+    return r
+
+
 for i in index_eyeBad:
     if(i >= 18):
-        r = random.randint(0, 17)
+        r = r_rand()
         swap = indexList[i]
         indexList[i] = indexList[r]
         indexList[r] = swap
 
-
-l = indexList
-for i in range(len(l)):
-    if(l[i] == 30):
-        print("30は"+str(i))
-    if(l[i] == 31):
-        print("31は"+str(i))
-    if(l[i] == 43):
-        print("43は"+str(i))
-
-print(indexList)
-
+print("\n\t\t黒板側\n")
+for i in range(len(indexList)):
+    print(str(indexList[i]) + "\t", end="")
+    if((i+1) % 6 == 0):
+        print("")
+print("\n")
