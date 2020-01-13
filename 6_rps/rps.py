e91da341e9f556f09d2c05f2328c5d471fe2c5c8
terfno <sueda.takahito@gmail.com>
2020-01-13 23:11:41 +0900

    :bug::fire::tada: fix bug and update

--- 6_rps/rps.py
+++ 6_rps/rps.py
@@ -5,7 +5,7 @@ import random
 import numpy
 import copy
 
-Pattern = numpy.zeros(3**3, 3)  # 履歴2戦略用のテーブル
+Pattern = [0]*(3**3)  # 履歴2戦略用のテーブル
 
 
 def hand_random(hands):  # でたらめ戦略
@@ -25,7 +25,7 @@ def hand_if(hands, jadged):  # 場合分け戦略
     return hands[jadge]  # draw:0->rock, lose:1->paper, win:2->scissors
 
 
-def hand_hist(st, hands):  # 履歴2戦略
+def hand_hist(hands, st):  # 履歴2戦略
     global Pattern
 
     if len(st) == 3:  # patternの更新
@@ -34,10 +34,17 @@ def hand_hist(st, hands):  # 履歴2戦略
         _ = st.pop()
 
     if len(st) == 2:  # 履歴2戦略ロジック部分
+        stg = copy.copy(st)
+        stg.append('g')
+        stp = copy.copy(st)
+        stp.append('p')
+        stt = copy.copy(st)
+        stt.append('t')
+
         should = [
-            Pattern[find_row(st.append("g"))],
-            Pattern[find_row(st.append("p"))],
-            Pattern[find_row(st.append("t"))]
+            Pattern[find_row(stg)],
+            Pattern[find_row(stp)],
+            Pattern[find_row(stt)]
         ]
         return hands[should.index(max(should))]
     elif len(st) < 2:  # 履歴が足りないときはランダム
@@ -46,7 +53,7 @@ def hand_hist(st, hands):  # 履歴2戦略
 
 def find_row(past_t):  # 行特定->三進数から十進数への変換してるだけ
     index = 0
-    for i in range(past_t):
+    for i in range(len(past_t)):
         if past_t[i] == "g":
             coef = 0
         elif past_t[i] == "p":
@@ -66,9 +73,9 @@ def jadge(my, enemy):  # 判定
         return 2
 
 
-def add_point(jadge, point):  # ポイント加算
+def add_point(jadged, point):  # ポイント加算
     add = [5, -3, 2]  # [draw, lose, win]
-    return point + add[jadge]
+    return point + add[jadged]
 
 
 def main():
@@ -84,15 +91,16 @@ def main():
     point = 0
 
     for _ in range(times):
-        myHand = hand_random(hands)
+        myHand = hand_hist(hands, past_t)
         print("myhand:" + myHand)
 
         enemyHand = input("Enemy's Hand(g:rock,p:paper,t:scissors): ")
 
         past.append(enemyHand)
+        past_t.append(enemyHand)
         jadged = jadge(myHand, enemyHand)
 
-        point = add_point(jadge, point)
+        point = add_point(jadged, point)
 
     print(point)
 
